ba11fc1acef860e244602f2b9375957b9fe1885a
shariquearshad <shariquearshad@gmail.com>
2020-12-17 22:24:12 +0530

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
+import Heading from "./components/Heading";
+import InputQuery from "./components/InputQuery";
+import SubHeading from "./components/SubHeading";
+import SubmitButton from "./components/SubmitButton";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <>
+    <App />
+    <Heading />
+    <InputQuery />
+    <SubHeading />
+    <SubmitButton />
+  </>,
+  document.getElementById("root")
+);
